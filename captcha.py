976694bc62fc914d19776de7e88d0a2c741f76a2
Lab-Brat <xoovoox@protonmail.com>
2022-09-25 13:52:20 +0300

    add resize

--- captcha.py
+++ captcha.py
@@ -10,14 +10,13 @@ class Captcha:
     def __init__(self, image_path):
         self.digit = self.captcha_to_digit(image_path)
 
-        # image processing parameters
-        self.threshold = 230
-        self.kernel_size = (0, 0)
-        self.sigma = 2
+    def _resize(self, image, new_size):
+        (h, w) = image.shape[:2]
+        return cv2.resize(image, (w*new_size, h*new_size))
 
-    def _gray_to_blackwhite(self, image, write=False):
+    def _gray_to_blackwhite(self, image, lower_thresh, write=False):
         gray_im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        (thresh, bw_im) = cv2.threshold(gray_im, 230, 255, cv2.THRESH_BINARY)
+        bw_im = cv2.threshold(gray_im, lower_thresh, 255, cv2.THRESH_BINARY)[1]
 
         if write == True:
             cv2.imwrite('gr.jpeg', gray_im)
@@ -26,10 +25,17 @@ class Captcha:
         return bw_im
 
     def captcha_to_digit(self, image_path):
-        image_raw = cv2.imread(image_path)
-        image = cv2.GaussianBlur(image_raw, (0, 0), 2)
-        bw_im = self._gray_to_blackwhite(image)
-        numbers = pytesseract.image_to_string(bw_im, config="tessedit_char_whitelist=0123456789")
+        new_size = 4
+        lower_thresh = 230
+        kernel_size = (0, 0)
+        sigma = 7
+
+        image = cv2.imread(image_path)
+        image = self._resize(image, new_size)
+        image = cv2.GaussianBlur(image, kernel_size, sigma)
+        bw_im = self._gray_to_blackwhite(image, lower_thresh, write=True)
+
+        numbers = pytesseract.image_to_string(bw_im)
         return ''.join(c for c in numbers if c.isdigit())
  
 
@@ -37,3 +43,4 @@ if __name__ == '__main__':
     for i in range(1,6):
         cap = Captcha(f'captcha/{i}.jpeg')
         print(cap.digit)
+        break
