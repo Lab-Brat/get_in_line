e138faafe35b9408c8c3a787fa03923c3abbb673
Lab-Brat <xoovoox@protonmail.com>
2022-10-02 22:08:01 +0300

    fill out the form, solve captcha and press enter

--- browser.py
+++ browser.py
@@ -1,10 +1,11 @@
 import sys
 from captcha import Captcha
+from PIL import Image
+from io import BytesIO
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.firefox import GeckoDriverManager
-
 
 class Browser():
     def __init__(self, config, destination):
@@ -26,10 +27,24 @@ class Browser():
             file.write(self.driver.page_source)
         self.driver.close()
 
+    def get_captcha(self):
+        captcha = self.driver.find_element("id", self.config['fields']['cap'])
+        location, size = captcha.location, captcha.size
+        im = Image.open(BytesIO(self.driver.get_screenshot_as_png()))
+        left, top = location['x'], location['y']
+        right, bottom = location['x'] + size['width'], location['y'] + size['height']
+        im = im.crop((left, top, right, bottom))
+        im.save('data/_tmp_screenshot.png')
+
     def automate(self):
         input_name = self.driver.find_element("name", self.config['fields']['num'])
-        self.driver.execute_script('arguments[0].value="Blorblespack"', input_name)
+        self.driver.execute_script(f'arguments[0].value="{self.config["user"]["num"]}"', input_name)
         input_code = self.driver.find_element("name", self.config['fields']['pas'])
-        self.driver.execute_script('arguments[0].value="Smorkleplack"', input_code)
-        input_name.submit()
+        self.driver.execute_script(f'arguments[0].value="{self.config["user"]["pas"]}"', input_code)
+
+        input_captcha = self.driver.find_element("name", self.config['fields']['cap_input'])
+        self.get_captcha()
+        solver = Captcha('data/_tmp_screenshot.png', write=False)
+        input_captcha.send_keys(solver.solve(method='2captcha'))
+        input_captcha.send_keys(Keys.ENTER)
         # self.driver.close()
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from browser import Browser
 config = configparser.ConfigParser()
 config.read('data/_info')
 
-bb = Browser(config, 'ist')
+bb = Browser(config, 'trb')
 # bb.write_html()
 bb.automate()
