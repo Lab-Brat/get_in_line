408b620ccad006cc7c86fcf918810556999d59b7
Lab-Brat <xoovoox@protonmail.com>
2022-10-02 16:34:30 +0300

    add destination choice with match case

--- browser.py
+++ browser.py
@@ -1,3 +1,4 @@
+import sys
 from captcha import Captcha
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -6,26 +7,29 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 class Browser():
-    def __init__(self, config):
+    def __init__(self, config, destination):
         self.config  = config
+        match destination:
+            case 'trb':
+                url = self.config['url']['trb']
+            case 'ist':
+                url = self.config['url']['ist']
+            case _:
+                print('Invalid URL. Available options: [trb, ist]')
+                sys.exit()
         self.service = Service(GeckoDriverManager().install())
         self.driver  = webdriver.Firefox(service = self.service)
+        self.driver.get(url)
 
-    def write_html(self, content):
+    def write_html(self):
         with open('source.html', 'w') as file:
-            file.write(content)
+            file.write(self.driver.page_source)
+        self.driver.close()
 
-    def get(self, write=False):
-        self.driver.get(self.config['url']['trb'])
-
+    def automate(self):
         input_name = self.driver.find_element("name", self.config['fields']['num'])
         self.driver.execute_script('arguments[0].value="Blorblespack"', input_name)
         input_code = self.driver.find_element("name", self.config['fields']['pas'])
         self.driver.execute_script('arguments[0].value="Smorkleplack"', input_code)
         input_name.submit()
-
-        if write == True:
-            self.write_html(self.driver.page_source)
-            print("HTML file written")
-        
         # self.driver.close()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import configparser
-from captcha import Captcha
 from browser import Browser
 
 config = configparser.ConfigParser()
 config.read('info')
 
-bb = Browser(config)
-bb.get(write=False)
+bb = Browser(config, 'ist')
+bb.write_html()
+# bb.automate()
