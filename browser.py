dcedf00d7dea5922a0ab02ad6cdc142a66bdc00a
Lab-Brat <xoovoox@protonmail.com>
2022-10-02 22:24:17 +0300

    stylistic changes

--- browser.py
+++ browser.py
@@ -22,29 +22,37 @@ class Browser():
         self.driver  = webdriver.Firefox(service = self.service)
         self.driver.get(url)
 
+        self.field_num = self.config["fields"]["num"]
+        self.field_pas = self.config["fields"]["pas"]
+        self.field_cap = self.config["fields"]["cap"]
+        self.field_cin = self.config["fields"]["cap_input"]
+
+        self.user_num = self.config["user"]["num"]
+        self.user_pas = self.config["user"]["pas"]
+
     def write_html(self):
         with open('data/_source.html', 'w') as file:
             file.write(self.driver.page_source)
         self.driver.close()
 
     def get_captcha(self):
-        captcha = self.driver.find_element("id", self.config['fields']['cap'])
+        captcha = self.driver.find_element("id", self.field_cap)
         location, size = captcha.location, captcha.size
         im = Image.open(BytesIO(self.driver.get_screenshot_as_png()))
-        left, top = location['x'], location['y']
-        right, bottom = location['x'] + size['width'], location['y'] + size['height']
+        left, right = location['x'], location['x'] + size['width']
+        top, bottom = location['y'], location['y'] + size['height']
         im = im.crop((left, top, right, bottom))
         im.save('data/_tmp_screenshot.png')
 
     def automate(self):
-        input_name = self.driver.find_element("name", self.config['fields']['num'])
-        self.driver.execute_script(f'arguments[0].value="{self.config["user"]["num"]}"', input_name)
-        input_code = self.driver.find_element("name", self.config['fields']['pas'])
-        self.driver.execute_script(f'arguments[0].value="{self.config["user"]["pas"]}"', input_code)
+        input_name = self.driver.find_element("name", self.field_num)
+        input_code = self.driver.find_element("name", self.field_pas)
+        self.driver.execute_script(f'arguments[0].value="{self.user_num}"', input_name)
+        self.driver.execute_script(f'arguments[0].value="{self.user_pas}"', input_code)
 
-        input_captcha = self.driver.find_element("name", self.config['fields']['cap_input'])
+        input_captcha = self.driver.find_element("name", self.field_cin)
         self.get_captcha()
         solver = Captcha('data/_tmp_screenshot.png', write=False)
-        input_captcha.send_keys(solver.solve(method='2captcha'))
+        input_captcha.send_keys(solver.solve(method='local'))
         input_captcha.send_keys(Keys.ENTER)
         # self.driver.close()
